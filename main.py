138362cd874ff97900223b03e086226d184f16b2
nghiapp7018 <nghia.pham@co-well.vn>
2022-09-23 06:01:58 +0700

    add manual guide and my_id command

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from asyncio import sleep
 import os
 from os import path
 import hashlib
 import logging
+import asyncio
 from datetime import time
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, JobQueue
@@ -16,7 +18,10 @@ from write_log import write_activity_log
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 DB_NAME = os.getenv('DB_NAME')
-ADMIN_TELEGRAM_ID = int(os.getenv('ADMIN_TELEGRAM_ID'))
+ALERT_INTERVAL = int(os.getenv('ALERT_INTERVAL'))
+ADMINS_TELEGRAM_ID = []
+for id in os.getenv('ADMINS_TELEGRAM_ID').split(','):
+    ADMINS_TELEGRAM_ID.append(int(id.strip()))
 
 # logging.basicConfig(
 #     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -25,17 +30,21 @@ ADMIN_TELEGRAM_ID = int(os.getenv('ADMIN_TELEGRAM_ID'))
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='Register a alert with format:\n `/percent_alert symbol screener exchange percent`.\nExp: /percent_alert ETHUSDT crypto BINANCE 5.\nYou can search on https://tvdb.brianthe.dev to see which symbol, exchange, and screener to use')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='1. Show your id: `/my_id\n2. Register a alert with format:\n `/register_alert symbol screener exchange percent`.\n\tExp: /register_alert ETHUSDT crypto BINANCE 5.\n\tYou can search on https://tvdb.brianthe.dev to see which symbol, exchange, and screener to use.\n3. Show alerts: `/show_alerts`\n4. Delete alert: `/delete_alert symbol screener exchange percent`')
+
+
+async def my_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.effective_chat.id)
 
 
 async def percent_alert(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """ 
-        command exp: /percent_alert TSLA america NASDAQ 5 
-        command exp: /percent_alert ETHUSDT crypto BINANCE 1 
+        command exp: /register_alert TSLA america NASDAQ 5 
+        command exp: /register_alert ETHUSDT crypto BINANCE 1 
         context.args = ['TSLA', 'america, 'NASDAQ', '5'] 
     """
     chat_id = update.effective_chat.id
-    if chat_id == ADMIN_TELEGRAM_ID:
+    if chat_id in ADMINS_TELEGRAM_ID:
         symbol, screener, exchange, percent = context.args
         alert = register_percent_alert(
             chat_id, symbol, screener, exchange, percent)
@@ -58,7 +67,7 @@ async def show_alerts(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if len(alerts):
         msg = 'List of alerts:'
         for index, alert in enumerate(alerts):
-            msg += f"\n{str(index + 1)}. {alert.symbol} - {alert.screener} - {alert.exchange} - {alert.percent}"
+            msg += f"\n{str(index + 1)}. {alert.symbol} {alert.screener} {alert.exchange} {alert.percent}"
         await context.bot.send_message(chat_id=chat_id, text=msg)
     else:
         await context.bot.send_message(chat_id=chat_id, text="You do not have any alerts.")
@@ -127,7 +136,7 @@ def load_all_alerts_and_create_job(job_queue: JobQueue):
     if len(alerts):
         for alert in alerts:
             job_queue.run_repeating(
-                alert_callback, interval=60, first=5, chat_id=alert.chat_id, name=str(alert.id), data=alert)
+                alert_callback, interval=ALERT_INTERVAL, first=5, chat_id=alert.chat_id, name=str(alert.id), data=alert)
             msg = f"Registed your alert for {alert.symbol} at {alert.percent}% successfully."
             write_activity_log(msg)
 
@@ -140,11 +149,13 @@ if __name__ == '__main__':
         load_all_alerts_and_create_job(application.job_queue)
 
     start_handler = CommandHandler('start', start)
+    myid_handler = CommandHandler('my_id', my_id)
     percent_alert_handler = CommandHandler('register_alert', percent_alert)
     show_alerts_handler = CommandHandler('show_alerts', show_alerts)
     delete_alert_handler = CommandHandler('delete_alert', delete_alert)
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
     application.add_handler(start_handler)
+    application.add_handler(myid_handler)
     application.add_handler(percent_alert_handler)
     application.add_handler(show_alerts_handler)
     application.add_handler(delete_alert_handler)
